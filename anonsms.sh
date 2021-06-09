744a3254286ced6ac78795c54b0da5e4f591de3f
Danish-1122 <81579183+Danish-1122@users.noreply.github.com>
2021-06-09 19:47:50 +0530

    Add files via upload

---
+++ anonsms.sh
@@ -0,0 +1,8 @@
+pkg update && pkg update
+pkg install python
+pkg install python2
+pip install requests
+clear
+python3 secret.py
+
+
---
+++ secret.py
@@ -0,0 +1,21 @@
+
+#author danish sheikh 
+#if you use this script then please give me the credits
+import os
+import time
+print('A simple python script to send anonymous \ntxt message to anyone in the entire world\n')
+print('Author danish sheikh\n')
+print('Best tools for sending anonymous txt message  within 18 lines of code\n')
+print('Important,use vps services to send anonymous sms many times \n you cant send a message to a number that you have send previously\n')
+print('you can send 1 sms to 1 number through 1 ip address and if you want to send more messages then change ip \n')
+print('please download hola vpn and send unlimited txt sms\n')
+num=(int(input('if you want to download  hola press 1\nand if you have  to downloaded then press 2 ')))
+if (num==1):
+ 
+       os.system('xdg-open https://hola.org')
+
+else:
+   
+     print('if you dont download hola vpn you cant send messages')
+
+import secret1
